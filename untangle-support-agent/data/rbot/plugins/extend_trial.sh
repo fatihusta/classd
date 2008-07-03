6a540c493ecade81869ba907f23cebc6ac7b91f9
Sebastien Delafond <sdelafond@gmail.com>
2008-07-03 17:55:13 +0000

    Remove warning about not finding either 30 or 14 days trial, and exit 0 in all cases; make script executable.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/pkgs@17206 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-support-agent/data/rbot/plugins/extend_trial.sh
+++ untangle-support-agent/data/rbot/plugins/extend_trial.sh
@@ -9,6 +9,10 @@ for t in /usr/share/untangle/bin/rup-* ; do
 done
 
 ## Run all of the libitems to decompress the keys, this will also reload the licenses.
-for t in /var/lib/dpkg/info/untangle-libitem-trial{14,30}-*.postinst ; do
+for t in `ls /var/lib/dpkg/info/untangle-libitem-trial{14,30}-*.postinst 2> /dev/null` ; do
   bash $t
 done
+
+#rush -c '%w(untangle-user-directory-integration untangle-policy-manager untangle-user-directory-management untangle-remote-access-portal untangle-configuration-backup untangle-dual-virus-blocker untangle-dual-virus-blocker-kav untangle-pcremote untangle-branding-manager).each { |n| puts "#{n} is expired: #{RUSH.uvm.license_manager.getLicenseStatus(n).is_expired}" }'
+
+exit 0
