8cd7c2138c18922c81a8396efe5583a8807698be
Robert Scott <rbscott@untangle.com>
2008-08-10 19:34:59 +0000

    Enabling single nic mode on install for the windows installer.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/pkgs@18133 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-arp-eater/files/var/lib/rails/untangle-net-alpaca/lib/alpaca/components/arp_eater_component.rb
+++ untangle-arp-eater/files/var/lib/rails/untangle-net-alpaca/lib/alpaca/components/arp_eater_component.rb
@@ -4,4 +4,37 @@ class Alpaca::Components::ArpEaterComponent < Alpaca::Component
       menu_organizer.register_item( "/main/advanced/snic", menu_item( 1000, "Single NIC", :action => "manage" ))
     end
   end
+
+  class Preconfiguration
+    def initialize( settings, networks )
+      @settings, @networks = settings, networks
+    end
+
+    attr_reader :settings, :networks
+  end
+
+  def pre_prepare_configuration( config, settings_hash )
+    settings = ArpEaterSettings.new( :enabled => false, :gateway => "auto", :broadcast => false ) 
+    network = ArpEaterNetworks.new( :enabled => false, :spoof => false, :ip => "0.0.0.0", :netmask => "0",
+                                    :opportunistic => false, :gateway => "auto", 
+                                    :description => "Local network" )
+
+    if ( config["single_nic_mode"] == true )
+      settings.enabled = true
+      network.enabled = true
+      network.spoof = true
+    end
+
+    settings_hash[self.class] = Preconfiguration.new( settings, [ network ] )
+  end
+
+  def pre_save_configuration( config, settings_hash )
+    ArpEaterSettings.destroy_all
+    ArpEaterNetworks.destroy_all
+
+    s = settings_hash[self.class]
+    
+    s.settings.save
+    s.networks.each { |n| n.save }
+  end
 end
