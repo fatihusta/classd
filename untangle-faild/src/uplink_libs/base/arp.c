1a5c9598592a8e3ce015e48034803a6a846d7778
Robert Scott <rbscott@untangle.com>
2009-03-05 02:13:41 +0000

    A few more updates.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/pkgs@22275 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-faild/src/uplink_libs/base/arp.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2003-2008 Untangle, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Untangle, Inc. ("Confidential Information"). You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <mvutil/debug.h>
+#include <mvutil/errlog.h>
+
+
+#include "uplink_libs/base/arp.h"
+
+static int _init( faild_uplink_test_instance_t *instance );
+static int _run( faild_uplink_test_instance_t *instance,
+                 struct in_addr* primary_address, struct in_addr* default_gateway );
+static int _cleanup( faild_uplink_test_instance_t *instance );
+static int _destroy( faild_uplink_test_instance_t *instance );
+
+/* Retrieve the class for arp */
+int faild_uplink_lib_base_arp_class( faild_uplink_test_class_t* test_class )
+{    
+    if ( faild_uplink_test_class_init( test_class, "arp", _init, _run, _cleanup, _destroy, NULL ) < 0 ) {
+        return errlog( ERR_CRITICAL, "faild_uplink_test_class_init\n" );
+    }
+
+    return 0;
+}
+
+static int _init( faild_uplink_test_instance_t *instance )
+{
+    return 0;
+}
+
+static int _run( faild_uplink_test_instance_t *instance,
+                 struct in_addr* primary_address, struct in_addr* default_gateway )
+{
+    return 0;
+}
+
+static int _cleanup( faild_uplink_test_instance_t *instance )
+{
+    return 0;
+}
+
+static int _destroy( faild_uplink_test_instance_t *instance )
+{
+    return 0;
+}
+
+
---
+++ untangle-faild/src/uplink_libs/base/arp.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2003-2009 Untangle, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Untangle, Inc. ("Confidential Information"). You shall
+ * not disclose such Confidential Information.
+ *
+ * $Id$
+ */
+
+#ifndef __ARP_H_
+#define __ARP_H_
+
+#include "faild.h"
+
+/* Retrieve the class for arp */
+int faild_uplink_lib_base_arp_class( faild_uplink_test_class_t* test_class );
+
+#endif // __ARP_H_
