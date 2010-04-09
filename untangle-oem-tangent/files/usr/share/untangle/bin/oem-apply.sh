0ca19dadeb7406cb1cca5689cebf64a9dfb33fb6
Dirk Morris <dmorris@untangle.com>
2010-04-09 18:43:05 +0000

    additioal customizations for tangent (Comment: #7599)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/pkgs@26236 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-oem-tangent/files/usr/share/untangle/bin/oem-apply.sh
+++ untangle-oem-tangent/files/usr/share/untangle/bin/oem-apply.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
 
+# apply files
 rsync -Ha /usr/share/untangle-oem-*/ /
+
+# rename grub titles
+sed -i 's|^\(title.*\)Debian GNU/Linux, kernel|\1Kernel|' /boot/grub/menu.lst
+sed -i 's|^\(title.*\)-untangle|\1|' /boot/grub/menu.lst
+
+# change default settings
+psql -U postgres uvm -c "update settings.u_mail_settings set from_address = 'webhawk@webhawk.untangle.com' where from_address = 'untangle@untangle.example.com'"
+psql -U postgres uvm -c "update settings.u_address_settings set hostname = 'webhawk.example.com' where hostname = 'untangle.example.com'"
