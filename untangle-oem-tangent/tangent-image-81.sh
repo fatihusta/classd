f32ec955e7137546df4b083da12b66e7309c2283
Marc Runkel <marc@runkel.org>
2011-03-03 23:32:49 +0000

    Modified Script to match new tangent product
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/pkgs@28541 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-oem-tangent/tangent-image-81.sh
+++ untangle-oem-tangent/tangent-image-81.sh
@@ -60,34 +60,16 @@ sleep 5
 # download apps
 #
 # Tangent libitem list:
-#untangle-libitem-adblocker
-#untangle-libitem-clam
-#untangle-libitem-cpd
-#untangle-libitem-firewall
-#untangle-libitem-ips
-#untangle-libitem-lite-package
-#untangle-libitem-openvpn
-#untangle-libitem-phish
 #untangle-libitem-protofilter
 #untangle-libitem-reporting
 #untangle-libitem-shield
-#untangle-libitem-spamassassin
-#untangle-libitem-spyware
-#untangle-libitem-adconnector
-#untangle-libitem-boxbackup
-#untangle-libitem-branding
-#untangle-libitem-faild
-#untangle-libitem-kav
-#untangle-libitem-policy
-#untangle-libitem-professional-package
 #untangle-libitem-sitefilter
-#untangle-libitem-splitd
-#untangle-libitem-support
+#untangle-libitem-cpd
 
 OTHER="untangle-support-agent"
-LIBITEMS="untangle-libitem-adblocker untangle-libitem-clam untangle-libitem-cpd untangle-libitem-firewall untangle-libitem-ips untangle-libitem-lite-package untangle-libitem-openvpn untangle-libitem-phish untangle-libitem-protofilter untangle-libitem-reporting untangle-libitem-shield untangle-libitem-spamassassin untangle-libitem-spyware untangle-libitem-webfilter untangle-libitem-adconnector untangle-libitem-boxbackup untangle-libitem-branding untangle-libitem-faild untangle-libitem-policy untangle-libitem-professional-package untangle-libitem-sitefilter untangle-libitem-support"
-RACK_NODES="untangle-node-clam untangle-node-firewall untangle-node-ips untangle-node-phish untangle-node-protofilter untangle-node-spamassassin untangle-node-spyware untangle-node-kav untangle-node-sitefilter"
-SERVICE_NODES="untangle-node-openvpn untangle-node-reporting untangle-node-adconnector untangle-node-boxbackup untangle-node-faild untangle-node-policy untangle-node-support untangle-node-shield"
+LIBITEMS="untangle-libitem-cpd untangle-libitem-protofilter untangle-libitem-reporting untangle-libitem-shield untangle-libitem-sitefilter"
+RACK_NODES="untangle-node-protofilter untangle-node-sitefilter"
+SERVICE_NODES="untangle-node-reporting untangle-node-reporting untangle-node-cpd untangle-node-shield"
 
 echo "apt-get install --yes --force-yes $OTHER"
 apt-get install --yes --force-yes $OTHER
@@ -96,7 +78,7 @@ echo "apt-get install --yes --force-yes $LIBITEMS"
 apt-get install --yes --force-yes $LIBITEMS
 
 if [ ! $? -eq 0 ] ; then
-    echo "ERROR: Failed to download libitems successfully - call Untangle or email dmorris@untangle.com"
+    echo "ERROR: Failed to download libitems successfully - call Untangle or email support@untangle.com"
     exit 1
 fi
 
@@ -117,7 +99,7 @@ for i in $RACK_NODES ; do
     echo "Installing $i" 
     TID="`ucli -p 'Default Rack' instantiate $i`"
     if [ ! $? -eq 0 ] ; then
-        echo "ERROR: Failed to install libitems successfully - call Untangle or email dmorris@untangle.com"
+        echo "ERROR: Failed to install libitems successfully - call Untangle or email support@untangle.com"
         exit 1
     fi    
     ucli start $TID &> /dev/null
@@ -128,7 +110,7 @@ for i in $SERVICE_NODES ; do
     echo "Installing $i" 
     TID="`ucli instantiate $i`"
     if [ ! $? -eq 0 ] ; then
-        echo "ERROR: Failed to install libitems successfully - call Untangle or email dmorris@untangle.com"
+        echo "ERROR: Failed to install libitems successfully - call Untangle or email support@untangle.com"
         exit 1
     fi    
     ucli start $TID &> /dev/null
