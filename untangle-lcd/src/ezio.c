02e46eb5fb0130b04fb31f0fe292dff60bb39fa6
Dirk Morris <dmorris@untangle.com>
2012-02-08 22:16:54 +0000

    add external/internal/disk to LCD daemon, and toggling on and off of auto-cycle, added auto-refresh also
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/hades/pkgs@31084 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-lcd/src/ezio.c
+++ untangle-lcd/src/ezio.c
@@ -8,18 +8,22 @@
 
 #define VERSION "1.0"
 #define LINE_LENGTH 16
-#define NUM_PAGES 3
+#define NUM_PAGES 6
 
 //com port. 0=COM1 or ttyS0
 #define COMPORT 1 
 #define BAUD 2400
 #define NULSTR "                                       "
 
+#define CYCLE_TIME_SECONDS 5
+#define REFRESH_TIME_SECONDS 10
+
 void ShowMessage (char *str1 , char *str2)
 {
     int a,b;
-    printf("LINE1: \"%s\"\n", str1);
-    printf("LINE2: \"%s\"\n", str2);
+    Cls();
+    //printf("DISPLAY LINE 1: \"%s\"\n", str1);
+    //printf("DISPLAY LINE 2: \"%s\"\n", str2);
     a = strlen(str1);
     b = 40 - a;
     write(fd,str1,a);
@@ -36,7 +40,7 @@ void getoutput(char* cmd, char* output, int outputlen)
     /* Open the command for reading. */
     fp = popen(cmd, "r");
     if (fp == NULL) {
-        printf("Failed to run command\n" );
+        fprintf( stderr,"Failed to run command\n" );
         return;
     }
 
@@ -67,12 +71,9 @@ void showpage(int page)
     bzero(line1,LINE_LENGTH+1);
     bzero(line2,LINE_LENGTH+1);
 
-    printf("Showing page: %i\n",page);
-
     snprintf(cmd1,255,"/usr/share/untangle-lcd/bin/ut-lcd-page %i %i",page,1);
     snprintf(cmd2,255,"/usr/share/untangle-lcd/bin/ut-lcd-page %i %i",page,2);
 
-    Cls();
     getoutput(cmd1, line1, LINE_LENGTH);
     getoutput(cmd2, line2, LINE_LENGTH);
     ShowMessage(line1, line2);
@@ -85,47 +86,122 @@ int main(int argc, char* argv[]) {
     fprintf(stderr,"Untangle EZIO LCD-server " VERSION"\n"); 
     Serial_Init(COMPORT, BAUD);  /* Initialize RS-232 environment */ 
     Init(); 			/* Initialize EZIO */
-    Cls();			/* Clear screen */
-    init_all_icon();		/* Initialize all icon */
+    Cls();			    /* Clear screen */
+    init_all_icon();	/* Initialize all icon */
 
     unsigned char line1[16];
     unsigned char line2[16];
 
     int current_page = 0;
-    showpage(current_page);
-   
-    while (1) {			
-        int res;
-        unsigned char buf[255];
-     
-        ReadKey(); /* sub-routine to send "read key" command */
-        res = read(fd,buf,255); /* read response from EZIO */
+    int result;
+    time_t next_cycle_time = 0; /* next time that the page should be changed */
+    time_t next_refresh_time = 0; /* next time that the page should be refreshed */
+    int auto_cycling = 1;
+    unsigned char buf[255];
 
-        switch(buf[1]) {
-	    case 0xbe: 	/* Up Botton was received */
+    showpage(current_page);
+
+#if 0
+    // the API doesn't appear to support NONBLOCK
+    if (fcntl(fd,F_SETFL,O_NONBLOCK) < 0) {
+        fprintf(stderr,"Failed to set NONBLOCK: %s\n",strerror(errno));
+    }
+#endif
+    
+    while (1) {			
+
+        usleep(100000); //sleep .1 seconds
+        bzero(buf,255);
+        
+        /**
+         * If auto-cycle is on and the next cycle time isn't computed
+         * compute it.
+         * Also set the next_refresh time if necessary
+         */
+        if (auto_cycling && next_cycle_time == 0) {
+            next_cycle_time = time(NULL) + CYCLE_TIME_SECONDS;
+        }
+        if (next_refresh_time == 0) {
+            next_refresh_time = time(NULL) + REFRESH_TIME_SECONDS;
+        }
+
+        /**
+         * Read from the LCD
+         */
+        ReadKey(); 
+        if ((result = read(fd,buf,255)) <= 0) /* read response from EZIO */ {
+            if (result == 0) {
+                fprintf(stderr, "EOF from LCD device. Exiting...");
+                exit(1);
+            }
+            if (result < 0 && errno != EAGAIN) {
+                fprintf(stderr, "Read error from LCD device: \"%s\" Exiting...", strerror(errno));
+                exit(1);
+            }
+
+        }
+
+        /**
+         * Check auto-cycling settings,
+         * If its on and the cycle time has passed, cycle the page
+         */
+        if (auto_cycling && (time(NULL) >= next_cycle_time)) {
+            //printf("Auto-cycling current page...\n"); fflush(stdout);
             current_page = ((current_page + 1) % NUM_PAGES);
             showpage(current_page);
+            next_cycle_time = 0; /* reset next cycle time */
+        }
+
+        /**
+         * Process input from user
+         */
+        switch(buf[1]) {
+	    case 0xbe: 	/* Up Botton was received */
+            //printf("KEY: UP\n");
+            current_page = ((current_page + 1) % NUM_PAGES);
+            showpage(current_page);
+            next_refresh_time = 0; next_cycle_time = 0; /* reset counters */
             break;
 	 
         case 0xbd:	/* Down Botton was received */
+            //printf("KEY: DOWN\n");
             current_page = ((current_page - 1) % NUM_PAGES);
+            if (current_page < 0) current_page = NUM_PAGES-1;
             showpage(current_page);
+            next_refresh_time = 0; next_cycle_time = 0; /* reset counters */
             break;
 	 
         case 0xbb: /* Enter Botton was received */
-            current_page = 1;
+            //printf("KEY: ENTER\n");
+            auto_cycling = !auto_cycling; //toggle auto-cycle
+            if (auto_cycling) {
+                ShowMessage("** Auto-Cycle **","      ON        ");
+                sleep(3);
+            } else {
+                ShowMessage("** Auto-Cycle **","      OFF       ");
+                sleep(3);
+            }
             showpage(current_page);
+            next_refresh_time = 0; next_cycle_time = 0; /* reset counters */
             break;
 	     
         case 0xb7: /* Escape Botton was received */
-            current_page = 1;
+            //printf("KEY: ESC\n");
+            current_page = 0;
             showpage(current_page);
+            next_refresh_time = 0; next_cycle_time = 0; /* reset counters */
             break;
+        default:
+            /* otherwise just check the refresh time and continue */
+            if (time(NULL) >= next_refresh_time) {
+                //printf("REFRESH\n");
+                showpage(current_page);
+                next_refresh_time = 0;
+            }
         } 
  
     }
 
-    printf("Done.\n\n");
     Uninit_Serial();
     return 0;
  
